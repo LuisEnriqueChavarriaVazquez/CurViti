28eb0602a91ae1868068ade4869f24ba1ec9f773
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2020-04-28 14:03:22 -0500

    Validacion de ingreso aspirante

---
+++ ProyectoCurvity_principal/validarPrimParteRegistroAspirante.php
@@ -0,0 +1,127 @@
+<?php
+   $nombreAs=$_POST["nombre_aspirante"];
+   $apelPatAs=$_POST["apellido_paterno"];
+   $apelMatAs=$_POST["apellido_materno"];
+   $mailAs=$_POST["mail"];
+   $passwordAs=$_POST["password"];
+   $fechaNacAs=$_POST["fecha_nac"];
+   $nacionalidadAs=$_POST["nacionalidad"];
+   $escuelaAs=$_POST["alama_mater"];
+   $nivelAcAs=$_POST["nivel_acad"];
+   $direccionAs=$_POST["direccion_aspirante"];
+   $telAs=$_POST["tel_aspirante"];
+   $facebookAs=$_POST["facebook"];
+   $skypeAs=$_POST["skype"];
+   $twitterAs=$_POST["twitter"];
+   $contadorEleConfimados=0;
+  
+ function validacionNormal ($StringEntrada){
+   if(empty($StringEntrada) || $StringEntrada==''){
+      return False;
+   }else{
+      return True;
+   }
+}
+
+function validacionMail ($StringEntrada){
+   if(empty($StringEntrada) || $StringEntrada==''){
+      return False;
+   }elseif(!filter_var($StringEntrada, FILTER_VALIDATE_EMAIL)){
+      return False;
+   }else{
+      return True;
+   }
+}
+
+function validacionTel ($StringEntrada){
+   if(empty($StringEntrada) || $StringEntrada==''){
+      return False;
+   }elseif(!preg_match("[0-9]+",$StringEntrada)){
+      return False;
+   }else{
+      return True;
+   }
+}
+
+
+   if(!validacionNormal($nombreAs)){
+       $Nombre_error="Nombre invalido";
+   }else{
+      $contadorEleConfimados++;
+   }
+
+   if(!validacionNormal($apelPatAs)){
+      $apPaterno_error="Apellido Paterno invalido";  
+  }else{
+   $contadorEleConfimados++;
+  }
+
+  if(!validacionNormal($apelMatAs)){
+      $apMaterno_error="Apellido Materno invalido";
+  }else{
+   $contadorEleConfimados++;
+  }
+
+  if(!validacionNormal($mailAs)){
+   $mail_error="Correo invalido";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($passwordAs)){
+   $password_error="Password invalida";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($fechaNacAs)){
+   $fecha_error="Fecha nacimiento invalida";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($nacionalidadAs)){
+   $nacionalidad_error="Nacionalidad invalida";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($escuelaAs)){
+   $escuela_error="Escuela invalida";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($nivelAcAs)){
+   $nivelAcad_error="Nivel Acad&eacute;mico invalido";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($direccionAs)){
+   $direccion_error="Diecci&oacute;n invalida ";
+}else{
+   $contadorEleConfimados++;
+}
+
+if(!validacionNormal($telAs)){
+   $tel_error="Tel&eacute;fono invalido";
+}else{
+   $contadorEleConfimados++;
+}
+
+   if($contadorEleConfimados==11){
+      $_SESSION["nombreAs"]=$nombreAs;
+      $_SESSION["apPatAs"]=$apelPatAs;
+      $_SESSION["apMatAs"]=$apelMatAs;
+      $_SESSION["mailAs"]=$mailAs;
+      $_SESSION["passwordAs"]=$passwordAs;
+      $_SESSION["fechaNacAs"]=$fechaNacAs;
+      include("signUpAspirante2.php");
+   }else{
+      include("signUpAspirante.php");
+   }
+   
+
+
+?>
