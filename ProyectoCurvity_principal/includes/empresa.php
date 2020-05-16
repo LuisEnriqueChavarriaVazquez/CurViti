97a0d41dc3cf695c6be12e2b838ac2ebcd1cdc4c
ulisesJ01 <56226569+ulisesJ01@users.noreply.github.com>
2020-05-16 14:12:57 -0500

    inicio_sesion_empresa

--- ProyectoCurvity_principal/includes/empresa.php
+++ ProyectoCurvity_principal/includes/empresa.php
@@ -1,13 +1,13 @@
 <?php
 include 'db.php';
 
-class User extends DB{
+class Emp extends DB{
     private $nombre;
     private $username;
 
 
     public function userExists($user, $pass){
-        $query = $this->connect()->prepare('SELECT * FROM empresa WHERE CorreoElec = :user AND Contra = :pass');
+        $query = $this->connect()->prepare('SELECT * FROM empresa WHERE Email = :user AND Contra = :pass');
         $query->execute(['user' => $user, 'pass' => $pass]);
 
         if($query->rowCount()){
@@ -18,12 +18,12 @@ class User extends DB{
     }
 
     public function setUser($user){
-        $query = $this->connect()->prepare('SELECT * FROM aspirante WHERE CorreoElec = :user');
+        $query = $this->connect()->prepare('SELECT * FROM empresa WHERE Email = :user');
         $query->execute(['user' => $user]);
         
         foreach ($query as $currentUser) {
             $this->nombre = $currentUser['Nombre'];
-            $this->username = $currentUser['CorreoElec'];
+            $this->username = $currentUser['Email'];
         }
     }
 
